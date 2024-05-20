e2ddf812cdc1aae25a4a03d8afa2d4db5624b6a6
zdthns <zdthns25@gmail.com>
2024-05-20 16:46:44 +0300

    new start

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '../Components/Forms/Login/LoginForm';
+import styles from './styles.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
