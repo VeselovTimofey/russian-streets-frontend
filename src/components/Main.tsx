71b90fded5e5c97d43daa2b1a0b6319af3f6f259
VeselovTimofey <vestimofey@mail.ru>
2024-05-26 16:22:32 +0300

    feat: add members

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import Intro from './Intro.tsx';
 import Map from './Agency.tsx';
+import Members from './Members.tsx';
 
 function Main() {
   return (
     <main className="main">
       <Intro />
       <Map />
+      <Members />
     </main>
   );
 }
