9b7cac315bbbd5d75f7236c3eba5ce3141ee0366
zdthns <zdthns25@gmail.com>
2024-05-27 19:24:21 +0300

    fix

--- src/components/Contents/Contents.tsx
+++ src/components/Contents/Contents.tsx
@@ -1,13 +1,6 @@
 import { useAppSelector } from '../../service/hooks/hooks';
 import styles from './styles.module.scss';
 
-const data = {
-  images_url:
-    'https://previews.123rf.com/images/nikitafedorov/nikitafedorov2005/nikitafedorov200500085/147983908-young-nice-woman-has-a-bike-ride-in-a-park-on-sunset-copy-space.jpg',
-  name: 'zxc',
-  description: 'gala plrgmkolp mhgbdpokg',
-};
-
 const Contents = () => {
   const { name, description, images_url } = useAppSelector(
     (state) => state.discipline.discipline
